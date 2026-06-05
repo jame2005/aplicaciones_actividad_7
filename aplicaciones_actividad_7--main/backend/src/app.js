712e5f2926a128001b4d8859cf77179a40a51ba3
MARIAJOSE2005 <mintriago6546@utm.edu.ec>
2026-06-04 21:30:29 -0500

    Add rutaRoutes to API endpoint

--- aplicaciones_actividad_7--main/backend/src/app.js
+++ aplicaciones_actividad_7--main/backend/src/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 
+const rutaRoutes = require("./routes/rutaRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/rutas", rutaRoutes);
 
 module.exports = app;
